5f036113689a85c7242ce7d699af48874b05d812
YaeJuneJeong <jyj98071@gmail.com>
2021-05-20 16:01:47 +0900

    modify

--- GUi.py
+++ GUi.py
@@ -2,15 +2,15 @@ import time
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QThread, pyqtSignal
-from PyQt5.QtWidgets import QMessageBox, QMainWindow
+from PyQt5.QtWidgets import QMessageBox
 import requests
 import socket
 import cv2
 import pyrealsense2 as rs
 import numpy as np
 
-url_register = "http://54.210.105.132/api/myfarm/register/ip"
-url_info = "http://54.210.105.132/api/myfarm/info"
+url_register = "http://52.207.211.251/api/myfarm/register/ip"
+url_info = "http://52.207.211.251/api/myfarm/info"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(('8.8.8.8', 1))  # connect() for UDP doesn't send packets
@@ -24,20 +24,25 @@ profile = pipeline.start(config)
 
 user_id = 0
 
+ret = 0
+
+
 class Send(QThread):
     error = pyqtSignal(int)
+    info = pyqtSignal(dict)
     finished = pyqtSignal()
 
     def __init__(self, parent=None):
         super().__init__()
         self.main = parent
         self.isRun = False
+        global ret
 
     def run(self):
         try:
             while self.isRun:
                 # change ip address
-                data = {'id': 14, 'ip': '141414441311.12'}
+                data = {'id': 1, 'ip': local_ip_address}
                 response = requests.put(url_register, data=data, timeout=10)
                 print(response.status_code)
 
@@ -45,8 +50,10 @@ class Send(QThread):
                     raise Exception
                 while True:
 
-                    response = requests.get(url_info, params={'id': 14})
-                    if(response.json()['machine_userid']) is not 1:
+                    response = requests.get(url_info, params={'id': 1})
+
+                    if (response.json()['machine_userid']) is not 1:
+                        self.info.emit(response.json())
                         break
                     time.sleep(10)
 
@@ -103,22 +110,23 @@ class Window1(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super(Window1, self).__init__(parent)
 
+        self.secondWindow = Window2()
         self.label = RotateMe(alignment=QtCore.Qt.AlignCenter)
         self.label.set_pixmap(QtGui.QPixmap('./operating.png'))
 
         self.send = Send(self)
         self.send.finished.connect(self.stop)
         self.send.finished.connect(self.change_stack)
+        self.send.info.connect(self.get_info)
         self.send.error.connect(self.error)
 
-        button = QtWidgets.QPushButton('Submit')
+        button = QtWidgets.QPushButton('ip 저장')
         button.clicked.connect(self.label.start_animation)
         button.clicked.connect(self.start)
 
         lay = QtWidgets.QVBoxLayout(self)
         lay.addWidget(self.label)
         lay.addWidget(button)
-
 
     def start(self):
         if not self.send.isRun:
@@ -138,17 +146,34 @@ class Window1(QtWidgets.QWidget):
     def change_stack(self):
         self.parent().stack.setCurrentIndex(1)
 
+    def get_info(self,data):
+        self.parent().change(data)
+        print(data)
+
+
+
 
 class Window2(QtWidgets.QWidget):
 
     def __init__(self, parent=None):
         super(Window2, self).__init__(parent)
 
+        self.layout = QtWidgets.QVBoxLayout(self)
+
+    def set(self,data):
+        # print(data)
+        label = QtWidgets.QLabel(str(data['id']))
+        self.layout.addWidget(label)
+
+        label = QtWidgets.QLabel(str(data['machine_ip']))
+        self.layout.addWidget(label)
+
+        label = QtWidgets.QLabel(str(data['machine_name']))
+        self.layout.addWidget(label)
+
         button = QtWidgets.QPushButton('배지 확인')
         button.clicked.connect(self.check)
-
-        layout = QtWidgets.QVBoxLayout(self)
-        layout.addWidget(button)
+        self.layout.addWidget(button)
 
     def check(self):
         frames = pipeline.wait_for_frames()
@@ -159,7 +184,7 @@ class Window2(QtWidgets.QWidget):
         recent_image = color_image[240:, 214:428]
         check_image = cv2.imread('./recent.jpg')[240:, 214:428]
 
-        cv2.imwrite('./check.jpg',color_image)
+        cv2.imwrite('./check.jpg', color_image)
         hist_recent = cv2.calcHist(recent_image, [1], None, [255], [0, 255])
         hist_check = cv2.calcHist(check_image, [1], None, [255], [0, 255])
         number = cv2.compareHist(hist_recent, hist_check, cv2.HISTCMP_CORREL)
@@ -174,20 +199,38 @@ class Window2(QtWidgets.QWidget):
             self.change_stack()
 
     def change_stack(self):
-        self.parent().stack.setCurrentIndex(0)
+        self.parent().stack.setCurrentIndex(2)
+
+
+class Window3(QtWidgets.QWidget):
+
+    def __init__(self, parent=None):
+        super(Window3, self).__init__(parent)
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        label = QtWidgets.QLabel('그래프 화면 입니다')
+        self.layout.addWidget(label)
 
 class MainWindow(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("버섯 GUI")
+        self.setWindowIcon(QtGui.QIcon('mushroom.jpg'))
         self.stack = QtWidgets.QStackedLayout(self)
         self.stack1 = Window1(self)
         self.stack2 = Window2(self)
+        self.stack3 = Window3(self)
         self.stack.addWidget(self.stack1)
         self.stack.addWidget(self.stack2)
+        self.stack.addWidget(self.stack3)
         self.show()
+
+    def change(self,data):
+        self.stack2.set(data)
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
     main = MainWindow()
+    # main = Window1()
     app.exit(app.exec_())
