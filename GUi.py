f46128f755b8a37dfc262ff8184ada28baf5cc47
YaeJuneJeong <jyj98071@gmail.com>
2021-06-20 17:54:52 +0900

    add_float

--- GUi.py
+++ GUi.py
@@ -113,7 +113,7 @@ prg_id = 0
 # 시작전 polling 코드
 class Start_before(QThread):
     errors = pyqtSignal(int)
-    signal = pyqtSignal(str,str)
+    signal = pyqtSignal(float,float)
     finished = pyqtSignal()
     global data
     global water_num
@@ -247,7 +247,7 @@ class Start_before(QThread):
                     hum = code[18: 20]
                     temp = code[38: 40]
                     socket_data(temp, hum)
-                    self.signal.emit(str(temp), str(hum))
+                    self.signal.emit(temp, hum)
                 if seconds - 2 <= hour <= seconds + 2:
 
                     if len(data) - 1 < serial_send_len:
@@ -643,8 +643,8 @@ class Window3(QtWidgets.QWidget):
 
     @pyqtSlot(str,str)
     def renewal(self,arg1,arg2):
-        self.data1.setText(arg1)
-        self.data2.setText(arg2)
+        self.data1.setText(str(float(arg1)))
+        self.data2.setText(str(float(arg2)))
         # self.data1.repaint(arg)
         # self.data2.repaint()
         # self.layout.repaint()
